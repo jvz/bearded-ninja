9aa249ffa101c274520ea03e9432532d40fb2661
Matt Sicker <matthew.sicker@ahold.com>
2013-09-28 16:27:09 -0500

    Added post destruction controller and links.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,13 @@ class PostsController < ApplicationController
     end
   end
   
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to posts_path
+  end
+  
   # security enforcement
   private
   def post_params
