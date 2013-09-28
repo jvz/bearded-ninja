3e2dc0ad868bf956c1901e99e50ad7be73445b12
Matt Sicker <matthew.sicker@ahold.com>
2013-09-28 14:24:25 -0500

    Initial Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeardedNinja::Application.config.secret_key_base = '01ecde08a7736660c1b9bf9d511f1336e13718ba213ed4933f45d20a240a4138c96b7bcf120391f7cd9500f697d10fa0bcd8ddf27090a0116b994e6d2a9ca678'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeardedNinja::Application.config.session_store :cookie_store, key: '_bearded-ninja_session'
