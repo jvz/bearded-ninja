f288d0432a9cd7c4deb9b8f8c5d8d8c5914ee3bd
Matt Sicker <matthew.sicker@ahold.com>
2013-09-29 15:56:29 -0500

    Added subroute for comments under posts.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ BeardedNinja::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :posts
+  resources :posts do
+    resources :comments # nested resource
+  end
 
   # You can have the root of your site routed with "root"
   root to: 'welcome#index'
