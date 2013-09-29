b8594dc36fe470d7742c8d129908e99d57b0811e
Matt Sicker <matthew.sicker@ahold.com>
2013-09-29 15:54:59 -0500

    Added many to one relation for comments/posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  has_many :comments
   validates :title, presence: true, length: { minimum: 5 }
   # here are alternate ways that line could have been done using
   # separate validators (also from TM bundle macros)
